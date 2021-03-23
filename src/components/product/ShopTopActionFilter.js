b1327ad6b651ceecba2023b42e8448ce566599dd
Kiet <72509022+wwwKiet@users.noreply.github.com>
2021-03-23 10:44:53 +0300

    filter update

--- src/components/product/ShopTopActionFilter.js
+++ src/components/product/ShopTopActionFilter.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import React, { Fragment } from "react";
-import ShopTopFilter from "./ShopTopFilter";
-import { toggleShopTopFilter } from "../../helpers/product";
+// import ShopTopFilter from "./ShopTopFilter";
+// import { toggleShopTopFilter } from "../../helpers/product";
 
 const ShopTopActionFilter = ({
   getFilterSortParams,
@@ -14,7 +14,7 @@ const ShopTopActionFilter = ({
     <Fragment>
       <div className="shop-top-bar mb-35">
         <div className="select-shoing-wrap">
-          <div className="shop-select">
+          {/* <div className="shop-select">
             <select
               onChange={e => getFilterSortParams("filterSort", e.target.value)}
             >
@@ -22,21 +22,21 @@ const ShopTopActionFilter = ({
               <option value="priceHighToLow">Price - High to Low</option>
               <option value="priceLowToHigh">Price - Low to High</option>
             </select>
-          </div>
+          </div> */}
           <p>
             Showing {sortedProductCount} of {productCount} result
           </p>
         </div>
 
-        <div className="filter-active">
+        {/* <div className="filter-active">
           <button onClick={e => toggleShopTopFilter(e)}>
             <i className="fa fa-plus"></i> filter
           </button>
-        </div>
+        </div> */}
       </div>
 
       {/* shop top filter */}
-      <ShopTopFilter products={products} getSortParams={getSortParams} />
+      {/* <ShopTopFilter products={products} getSortParams={getSortParams} /> */}
     </Fragment>
   );
 };
